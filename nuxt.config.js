c6342d56fe492305f61927b35069603b35803860
千葉工 <chibataku0815@gmail.com>
2019-08-25 18:40:00 +0900

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,64 @@
+export default {
+  mode: 'universal',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: {
+    color: '#fff'
+  },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [],
+  /*
+   ** Build configuration
+   */
+  build: {
+    extractCSS: true,
+    filenames: {
+      css: 'css/[name].css',
+      vendor: '[name].js',
+      app: 'app.[name].js',
+      chunk: '[name].js'
+    },
+    publicPath: '/assets/',
+    extend(config, ctx) {}
+  },
+  generate: {}
+}
